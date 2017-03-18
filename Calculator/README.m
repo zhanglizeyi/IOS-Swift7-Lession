153098fec8cc71437e6a79a31611a2fa07d9cd53
ze li <zhanglizeyi@users.noreply.github.com>
2017-03-18 02:40:30 -0700

    Update README.m

--- Calculator/README.m
+++ Calculator/README.m
@@ -29,7 +29,7 @@ The following **additional** features are implemented:
 
 Here's a walkthrough of implemented user stories:
 
-<img src='https://github.com/zhanglizeyi/IOS-Swift7-Lession/blob/master/Calculator/CalculatorProject.gif ' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+<img src=https://github.com/zhanglizeyi/IOS-Swift7-Lession/blob/master/Calculator/CalculatorProject.gif?raw=true title='Video Walkthrough' width='' alt='Video Walkthrough' />
 
 GIF created with [LiceCap](http://www.cockos.com/licecap/).
 
