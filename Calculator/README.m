aa9e74b29bb1ffcaa432b17d92295d7d5f8e473f
ze li <zhanglizeyi@users.noreply.github.com>
2017-03-18 02:39:09 -0700

    Update README.m

--- Calculator/README.m
+++ Calculator/README.m
@@ -1,10 +1,10 @@
-# Pre-work - *Name of App Here*
+# Pre-work - *Tip-Cal*
 
 **Name of your app** is a tip calculator application for iOS.
 
-Submitted by: **Your Name Here**
+Submitted by: **Ze Li**
 
-Time spent: **X** hours spent in total
+Time spent: **7** hours spent in total
 
 ## User Stories
 
@@ -13,21 +13,23 @@ The following **required** functionality is complete:
 * [x] User can enter a bill amount, choose a tip percentage, and see the tip and total values.
 
 The following **optional** features are implemented:
-* [ ] Settings page to change the default tip percentage.
-* [ ] UI animations
-* [ ] Remembering the bill amount across app restarts (if <10mins)
-* [ ] Using locale-specific currency and currency thousands separators.
-* [ ] Making sure the keyboard is always visible and the bill amount is always the first responder. This way the user doesn't have to tap anywhere to use this app. Just launch the app and start typing.
+* [X] Settings page to change the default tip percentage.
+* [X] UI animations
+* [] Remembering the bill amount across app restarts (if <10mins)
+* [] Using locale-specific currency and currency thousands separators.
+* [X] Making sure the keyboard is always visible and the bill amount is always the first responder. This way the user doesn't have to tap anywhere to use this app. Just launch the app and start typing.
 
 The following **additional** features are implemented:
 
-- [ ] List anything else that you can get done to improve the app functionality!
+- [X] List anything else that you can get done to improve the app functionality!
+- [X] Add more functionalities in there such as operations of a simple calculator
+- [X] Make higher version of the percetage control by setting page
 
 ## Video Walkthrough 
 
 Here's a walkthrough of implemented user stories:
 
-<img src='https://github.com/zhanglizeyi/IOS-Swift7-Lession/blob/master/Calculator/CalculatorProject.gif?raw=true' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+<img src='https://github.com/zhanglizeyi/IOS-Swift7-Lession/blob/master/Calculator/CalculatorProject.gif ' title='Video Walkthrough' width='' alt='Video Walkthrough' />
 
 GIF created with [LiceCap](http://www.cockos.com/licecap/).
 
